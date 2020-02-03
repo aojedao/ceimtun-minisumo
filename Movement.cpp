84ad969f24b2124f602678558379bbe666a61e70
aojedao <aojedao@unal.edu.co>
2020-02-03 15:11:07 -0500

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -1,1 +1,15 @@
-#Comment
+  #include "Movement.h"
+
+Movement::Movement(){
+  //Los pines digitales controlaran
+  motorA1_PIN=D4;
+  motorA2_PIN=D9;
+  motorB1_PIN=D7;
+  motorB2_PIN=D8;
+  
+  pinMode(motorA1_PIN,OUTPUT);
+  pinMode(motorA2_PIN,OUTPUT);
+  pinMode(motorB1_PIN,OUTPUT);
+  pinMode(motorB2_PIN,OUTPUT);
+}
+  
