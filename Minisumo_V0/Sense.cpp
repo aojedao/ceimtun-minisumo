d10a73b464873c4514c37ece2f79ff439ba4a430
DylanOM7 <60625359+DylanOM7@users.noreply.github.com>
2020-02-05 15:59:16 -0500

    Update Sense.cpp

--- Minisumo_V0/Sense.cpp
+++ Minisumo_V0/Sense.cpp
@@ -1,1 +1,26 @@
-//Comment
+  #include "Sense.h"
+
+
+Sense::Sense(){
+  //Los pines digitales controlaran
+  byte pin;
+  pinMode(pin , INPUT);
+  
+  USL_PIN=4; //UltraSoundLeft
+  USR_PIN=9;//UltraSoundRigth
+  USFL_PIN=7;//UltraSoundFrontLeft
+  USFR_PIN=8;//UltraSoundFrontRigth
+}
+
+
+void Sense::Location(){
+byte Distance =  analogRead (pin);
+ if(Distance > 50 && Distance < 150 ){
+  //movement.
+ }
+  
+}
+void Sense::Offside(){
+byte WB =  analogRead (pin);
+
+}
